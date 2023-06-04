b2a8bf23f6f1e64f95bd7c6fd678660872399924
AhmedKhaled6900 <you@example.com>
2023-06-04 08:43:22 +0300

    update css

--- src/layout/experts.js
+++ src/layout/experts.js
@@ -1,8 +1,52 @@
+import { Card, Col, Container, Row } from "react-bootstrap";
+import img1 from "../imgs/marketing01.png"
+import img2 from "../imgs/marketing02.png"
+import img3 from "../imgs/marketing03.png"
 function Experts() {
     return ( 
 
-<h1>experts</h1>
+<Container className=" animate__animated animate__fadeIn  --animate-duration: 2s mt-5">
+<Row>
 
+    <Col lg={4}>
+    <Card className="card" >
+
+        <img src={img1} alt="" />
+        <div className="card-layout" >
+
+<h3>
+Increasing Prosperity With Positive Thinking
+</h3>
+</div>
+    </Card>
+    </Col>
+    <Col lg={4}>
+    <Card className="card" >
+
+        <img src={img2} alt="" />
+        <div className="card-layout" >
+<h3>
+Increasing Prosperity With Positive Thinking
+</h3>
+</div>
+    </Card>
+
+    </Col>
+    <Col lg={4}>
+    <Card className="card" >
+
+        <img src={img3} alt="" />
+        <div className="card-layout" >
+
+<h3>
+Increasing Prosperity With Positive Thinking
+</h3>
+</div>
+    </Card>
+    </Col>
+  
+</Row>
+</Container>
      );
 }
 
